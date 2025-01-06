d78ff8d27022fdc9e27b4014a90652ca21bf86c8
Aaryan Chauhan <aaryan.chauhan.1007@gmail.com>
2025-01-06 10:56:58 +0530

    Add files via upload
    first day of fsd 1 learnt about the javascript loops and variables and functions etc

---
+++ day1/one.js
@@ -0,0 +1,34 @@
+document.write("<br>"+"Hello Wrold!")
+var a = 16+6+'xyz'
+var b="xyz"+ 16+6
+document.write("<br>"+"a = "+a+"<br>"+" b = "+b+"<br>")
+var a=12
+var b=true
+var c="ljkju"
+var d=""
+var e = {}
+document.write(typeof(a)+"<br>")
+document.write(typeof(b)+"<br>")
+document.write(typeof(c)+"<br>")
+document.write(typeof(z)+"<br>")
+document.write(typeof(e)+"<br>")
+
+var age=20
+if(age<=18){
+    document.write("hi"+"<br>")
+}else if(age>18 || age<=21){
+    document.write("bye"+"<br>")
+}else{
+    document.write("nikal"+"<br>")
+}
+
+var count=0
+while (count<10) {
+    document.write("count: "+(count)+"<br>")
+    count++
+}
+document.write("<br>")
+
+for(count=0;count<10;count++){
+    document.write("count: "+count+"<br>")
+}
